00c97e0cf1a4e3921671270b996865dba46c7b7f
Jasper Alblas <just_jasper@hotmail.com>
2021-05-06 16:23:33 +0200

    login and logout capability added

--- app.js
+++ app.js
@@ -26,12 +26,43 @@ app.set("views", __dirname);
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+    new LocalStrategy((username, password, done) => {
+        User.findOne({ username: username }, (err, user) => {
+        if (err) { 
+            return done(err);
+        };
+        if (!user) {
+            return done(null, false, { message: "Incorrect username" });
+        }
+        if (user.password !== password) {
+            return done(null, false, { message: "Incorrect password" });
+        }
+        return done(null, user);
+        });
+    })
+);
+
+passport.serializeUser(function(user, done) {
+done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+User.findById(id, function(err, user) {
+    done(err, user);
+});
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
 
-app.get("/", (req, res) => res.render("index"));
+app.get("/", (req, res) => {
+    res.render("index", { user: req.user });
+  });
+
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 
 app.post("/sign-up", (req, res, next) => {
@@ -45,5 +76,18 @@ app.post("/sign-up", (req, res, next) => {
       res.redirect("/");
     });
   });
+
+app.post(
+    "/log-in",
+    passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/"
+    })
+);
+
+app.get("/log-out", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
